d52faf3b28ad62333afa095d6667f008833c03d6
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2025-09-27 22:09:28 +0530

    updated animation for loading

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { useLoading } from '../context/LoadingContext';
+// 1. Import the Lottie Player component
+import { Player } from '@lottiefiles/react-lottie-player';
+// 2. Import your new animation JSON file
+import animationData from '../assets/loading-animation.json';
 
 const LoadingSpinner = () => {
     const { isLoading } = useLoading();
 
-    // The component will only render when isLoading is true
     if (!isLoading) {
         return null;
     }
 
     return (
+        // The overlay div is still used to dim the background
         <div className="spinner-overlay">
-            <div className="spinner-container"></div>
+            {/* 3. Use the Lottie Player instead of the old CSS spinner */}
+            <Player
+                autoplay
+                loop
+                src={animationData} // Use the imported animation data
+                style={{ height: '300px', width: '300px' }}
+            >
+            </Player>
         </div>
     );
 };
